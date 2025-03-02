ffeec27026d19c41d8b0d26bd18a71fa302e8558
Roman Volkov <rvolkovpost@gmail.com>
2025-03-02 12:35:00 +0200

    3.15-3.16

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
 
-app.use(express.json())
 app.use(cors())
 app.use(express.static('dist'))
+app.use(express.json())
 
 morgan.token('body', (req) => {
   return JSON.stringify(req.body)
@@ -39,17 +39,24 @@ app.get('/info', (request, response) => {
     `)
   })
 
-app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then(person => {
-      response.json(person)
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
     })
+    .catch(error => next(error))
   })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -77,6 +84,24 @@ app.post('/api/persons', (request, response) => {
     response.json(savedPerson)
   })
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT)
